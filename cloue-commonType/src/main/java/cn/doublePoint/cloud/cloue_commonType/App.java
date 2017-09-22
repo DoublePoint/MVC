93242aadec0f137ba5885b6de6b71ae080e747e4
DoublePoint <997820126@qq.com>
2017-09-22 23:11:31 +0800

    新提交文件Cloud-common cloud-commonType

---
+++ cloue-commonType/src/main/java/cn/doublePoint/cloud/cloue_commonType/App.java
@@ -0,0 +1,13 @@
+package cn.doublePoint.cloud.cloue_commonType;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
